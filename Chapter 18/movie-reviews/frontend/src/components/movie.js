961360ebe57af46489cb7fbf1dbba2ae45344b13
jnevin7 <144211993+jnevin7@users.noreply.github.com>
2024-07-05 14:12:24 +0200

    Add files via upload

---
+++ Chapter 18/movie-reviews/frontend/src/components/movie.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react'
+import MovieDataService from '../services/movies'
+import { Link, useParams } from 'react-router-dom'
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+
+const Movie = props => {
+    const { id } = useParams();
+    const [movie, setMovie] = useState({
+        id: null,
+        title: "",
+        rated: "",
+        reviews: []
+    })
+
+    const getMovie = id => {
+        MovieDataService.get(id)
+            .then(response => {
+                setMovie(response.data)
+                console.log(response.data)
+            })
+            .catch(e => {
+                console.log(e)
+            })
+    }
+
+    useEffect(() => {
+        getMovie(id);
+    }, [id]);
+
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col>
+                        <Image src={movie.poster + "/100px250"} fluid />
+                    </Col>
+                    <Col>
+                        <Card>
+                            <Card.Header as="h5">{movie.title}</Card.Header>
+                            <Card.Body>
+                                <Card.Text>
+                                    {movie.plot}
+                                </Card.Text>
+                                {props.user &&
+                                    <Link to={"/movies/" + props.match.params.id + "/review"}>
+                                        Add Review
+                                    </Link>}
+                            </Card.Body>
+                        </Card>
+                        <br></br>
+                        <h2>Reviews</h2>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
+export default Movie;
